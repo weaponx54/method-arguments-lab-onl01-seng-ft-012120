85357b159716d67c848b3b089a2dfeb4990bcdd0
Tyree Tomlinson <2reeree@gmail.com>
2020-01-23 15:22:08 +0000

    Automatically backed up by Learn

--- spec/introduction_spec.rb
+++ spec/introduction_spec.rb
@@ -7,7 +7,7 @@ describe "#introduction" do
 end
 
 describe "#introduction_with_language" do 
-  it "takes in two arguments, a name and a language, and puts out a phrase using those arguments." do 
-    expect{introduction_with_language("Dan", "Ember.js")}.to output("Hi, my name is Dan and I am learning to program in Ember.js.\n").to_stdout
+  it "takes in two arguments, a name and a language, and program in Ember.js.\n").to_stdout
   end
-end
+endputs out a phrase using those arguments." do 
+    expect{introduction_with_language("Dan", "Ember.js")}.to output("Hi, my name is Dan and I am learning to 

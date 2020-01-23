57df053a96fefb8734415509f9a11bf5fec0d40a
Tyree Tomlinson <2reeree@gmail.com>
2020-01-23 15:25:35 +0000

    Automatically backed up by Learn

--- spec/introduction_spec.rb
+++ spec/introduction_spec.rb
@@ -1,7 +1,5 @@
-require "spec_helper"
+introduction_with_language
 
-describe "#introduction" do 
-  it "takes in an argument of a name and puts out a phrase with that name using string interpolation" do 
-    expect{introduction("Josh")}.to output("Hi, my name is Josh.\n").to_stdout
-  end
+puts "hi my name is #{josh} and iam learning to program in #{ruby}"
 end
+ end

81c082a8f70ba9d233e7c7fc5a8d8be3660669ca
Tyree Tomlinson <2reeree@gmail.com>
2020-01-23 15:14:39 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,4 +1,4 @@
-introduction_with_language takes in twoarguments, a name and a language,
+introduction_with_language, a name and a language,
 
 def introduction(josh)
   puts "Hi, my name is #{josh}."
